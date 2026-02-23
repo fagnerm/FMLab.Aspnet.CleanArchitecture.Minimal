1d033133f405000630a2c2c28ca86cb7751ce4c0
Fagnerm <fagnermarinho.dev@gmail.com>
2026-02-23 16:54:59 -0300

    Organize Gateways and Repositories.

--- src/FMLab.Aspnet.CleanArchitecture.Application/Interfaces/Gateways/IUserGateway.cs
+++ src/FMLab.Aspnet.CleanArchitecture.Application/Interfaces/Gateways/IUserGateway.cs
@@ -5,6 +5,7 @@
 using FMLab.Aspnet.CleanArchitecture.Application.DTOs;
 using FMLab.Aspnet.CleanArchitecture.Application.Shared.Result;
 using FMLab.Aspnet.CleanArchitecture.Application.UseCases;
+using FMLab.Aspnet.CleanArchitecture.Domain.ValueObjects;
 
 namespace FMLab.Aspnet.CleanArchitecture.Application.Interfaces.Gateways;
 
@@ -12,4 +13,5 @@ public interface IUserGateway
 {
     Task<PageResult<UserSummaryDTO>> ListAsync(ListUsersFilter filter, CancellationToken token);
     Task<UserSummaryDTO?> ListUserByIdAsync(int id, CancellationToken token);
+    Task<bool> ExistsByKeyAsync(Name? name, Email? email, CancellationToken token);
 }
--- src/FMLab.Aspnet.CleanArchitecture.Application/Interfaces/Repositories/IUserRepository.cs
+++ src/FMLab.Aspnet.CleanArchitecture.Application/Interfaces/Repositories/IUserRepository.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT License. See LICENSE file in the project root for details.
 
 using FMLab.Aspnet.CleanArchitecture.Domain.Users;
-using FMLab.Aspnet.CleanArchitecture.Domain.ValueObjects;
 
 namespace FMLab.Aspnet.CleanArchitecture.Application.Interfaces.Repositories;
 
@@ -11,7 +10,6 @@ public interface IUserRepository
 {
     Task AddAsync(User user, CancellationToken token);
     void Delete(User user);
-    Task<bool> ExistsAsync(Name name, CancellationToken token);
     Task<User?> GetByIdAsync(int id, CancellationToken token);
     User Update(User user);
 }
