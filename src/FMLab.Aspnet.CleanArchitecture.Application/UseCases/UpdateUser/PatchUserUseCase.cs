459298f47d55d74877768059a272a023e055388b
Fagnerm <fagnermarinho.dev@gmail.com>
2026-02-24 11:52:20 -0300

    Remove inheritance from PathUser endpoint.

--- src/FMLab.Aspnet.CleanArchitecture.Application/UseCases/UpdateUser/PatchUserUseCase.cs
+++ src/FMLab.Aspnet.CleanArchitecture.Application/UseCases/UpdateUser/PatchUserUseCase.cs
@@ -1,25 +1,36 @@
-﻿// API - Clean architecture boilerplate
-// Copyright (c) 2026 Fagner Marinho 
+// API - Clean architecture boilerplate
+// Copyright (c) 2026 Fagner Marinho
 // Licensed under the MIT License. See LICENSE file in the project root for details.
 
 using FMLab.Aspnet.CleanArchitecture.Application.Interfaces;
 using FMLab.Aspnet.CleanArchitecture.Application.Interfaces.Repositories;
-using FMLab.Aspnet.CleanArchitecture.Domain.Entities;
+using FMLab.Aspnet.CleanArchitecture.Application.Shared.Result;
+using FMLab.Aspnet.CleanArchitecture.Application.Shared.UseCases;
 using FMLab.Aspnet.CleanArchitecture.Domain.ValueObjects;
 
 namespace FMLab.Aspnet.CleanArchitecture.Application.UseCases.UpdateUser;
 
-public class PatchUserUseCase : UpdateUserUseCase, IPatchUserUseCase
+public class PatchUserUseCase : TransactionalUseCaseBase<UpdateUserInputDTO, UpdateUserOutputDTO>, IPatchUserUseCase
 {
-    public PatchUserUseCase(IUnitOfWork unitOfWork, IUserRepository repository) : base(unitOfWork, repository)
+    private readonly IUserRepository _repository;
+
+    public PatchUserUseCase(IUnitOfWork unitOfWork, IUserRepository repository) : base(unitOfWork)
     {
+        _repository = repository;
     }
 
-    protected override Tuple<Name, Email?> MapUserUpdate(UpdateUserInputDTO input, ref User user)
+    public override async Task<Result<UpdateUserOutputDTO>> ExecuteHandlerAsync(UpdateUserInputDTO input, CancellationToken cancellationToken)
     {
+        var user = await _repository.GetByIdAsync(input.Id, cancellationToken);
+
+        if (user is null) return Result<UpdateUserOutputDTO>.NotFound("User not found");
+
         var name = input.Name is null ? user.Name : new Name(input.Name);
         var email = input.Email is null ? user.Email : new Email(input.Email);
+        user.Update(name, email);
 
-        return Tuple.Create(name, email);
+        _repository.Update(user);
+
+        return Result<UpdateUserOutputDTO>.Success();
     }
 }
